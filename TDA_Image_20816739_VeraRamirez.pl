a7d6bc807c9964f59fbe50b49dce2630a2ff3608
Cveramz <carlitos4044@gmail.com>
2022-11-02 23:38:58 -0300

    Paso de functores a archivos separados

--- TDA_Image_20816739_VeraRamirez.pl
+++ TDA_Image_20816739_VeraRamirez.pl
@@ -5,10 +5,61 @@
 
 %TDA IMAGE
 
+:-include(TDA_Pixbit_20816739_VeraRamirez).
 
 %--------------- Clausulas ---------------
 
 %Constructor de imagen
 
 imagen(Width,Height,Data, [Width,Height,Data]).
+
+
+%Pertenencia de Bitbit
+
+imageIsBitmap(Image):-
+    image(_,_,Data, Image),
+    isBitmap(Data).
+
+
+isBitmap([]).
+isBitmap([Pixbit|Rest]) :- 
+    pixbit(_,_, Bit, _, Pixbit),
+    (Bit == 0; Bit == 1),
+    isBitmap(Rest).
+
+%Pertenencia de pixrgb
+
+imageIsPixmap(Image):-
+    image(_,_,Data, Image),
+    isPixmap(Data).
+
+isPixmap([]).
+isPixmap([Pixrgb|Rest]) :- 
+    pixrgb(_,_, R, G, B, _, Pixrgb),
+    (R >= 0, R =< 255),
+    (G >= 0, G =< 255),
+    (B >= 0, B =< 255),
+    isPixmap(Rest).
+
+%Pertenencia de pixhex
+
+imageIsHexmap(Image):-
+    image(_,_,Data, Image),
+    isHexmap(Data).
+
+isHexmap([]).
+isHexmap([Pixhex|Rest]) :- 
+    pixhex(_,_, Hex, _, Pixhex),
+    %verificar si hex es un string
+    string(Hex),
+    isHexmap(Rest).
+
+%Verificar si image es comprimido.
+%Cuando está comprimido, la cantidad de pixeles es menor a la multiplicación entre Width y Height.
+
+imageIsCompressed(Image) :-
+    image(Width, Height, Data, Image),
+    length(Data, Length),
+    Length < Width * Height.
+
 
