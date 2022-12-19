4714bc25882566612283366f2f9c6788ee609559
CarlosVeRamz <83312809+Cveramz@users.noreply.github.com>
2022-12-19 17:24:27 -0300

    cambios finales

--- pruebas_20816739_VeraRamirez.pl
+++ pruebas_20816739_VeraRamirez.pl
@@ -10,6 +10,7 @@ EJEMPLO 2:
 pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I). 
 EJEMPLO 3:
 pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), image( 2, 2, [PA, PB], I).
+
 VERIFICADOR DE BITMAP
 EJEMPLO 1:
 pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageIsBitmap( I ).
@@ -19,22 +20,22 @@ EJEMPLO 3:
 pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 2, 20, PB), pixbit( 1, 0, 999, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageIsBitmap( I ).
 
 
-VERIFICADOR DE RGB
+VERIFICADOR DE RGB (PIXMAP)
 EJEMPLO 1:
 pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageIsPixmap( I ).
 EJEMPLO 2:
-pixhex(0,0,"#FF0000",PA), pixhex(0,1,"#00FF00",PB), pixhex(1,0,"#0000FF",PC), pixhex(1,1,"#FFFFFF",PD), image(2,2,[PA,PB,PC,PD],I), imageIsPixmap(I).
+pixhex(0,0,"#FF0000",0,PA), pixhex(0,1,"#00FF00",0,PB), pixhex(1,0,"#0000FF",0,PC), pixhex(1,1,"#FFFFFF",0,PD), image(2,2,[PA,PB,PC,PD],I), imageIsPixmap(I).
 EJEMPLO 3:
 pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2, [P1, P2, P3, P4], I), imageIsPixmap( I ).
 
 
-VERIFICADOR DE HEX
+VERIFICADOR DE HEXMAP
 EJEMPLO 1:
-pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageIsPixmap( I ).
+pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageIsHexmap( I ).
 EJEMPLO 2:
-pixhex(0,0,"#FF0000",PA), pixhex(0,1,"#00FF00",PB), pixhex(1,0,"#0000FF",PC), pixhex(1,1,"#FFFFFF",PD), image(2,2,[PA,PB,PC,PD],I), imageIsPixmap(I).
+pixhex(0,0,"#FF0000",0,PA), pixhex(0,1,"#00FF00",0,PB), pixhex(1,0,"#0000FF",0,PC), pixhex(1,1,"#FFFFFF",0,PD), image(2,2,[PA,PB,PC,PD],I), imageIsHexmap(I).
 EJEMPLO 3:
-pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2, [P1, P2, P3, P4], I), imageIsPixmap( I ).
+pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2, [P1, P2, P3, P4], I), imageIsHexmap( I ).
 
 
 VERIFICADOR DE COMPRESSED
@@ -45,6 +46,7 @@ pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), im
 EJEMPLO 3:
 pixhex(0,0,"#FF0000",1,PA), pixhex(0,1,"#00FF00",2,PB), pixhex(1,0,"#0000FF",3,PC), pixhex(1,1,"#FFFFFF",4,PD), image(2,2,[PA,PB,PC,PD],I), imageIsCompressed(I).
 
+
 FLIP HORIZONTAL
 EJEMPLO 1:
 pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageFlipH( I , I2 ).
@@ -52,6 +54,7 @@ EJEMPLO 2:
 pixbit( 0, 0, 0, 10, PA), pixbit( 0, 1, 0, 20, PB), image(1,2,[PA,PB],I), imageFlipH(I,I2).
 EJEMPLO 3:
 pixbit( 0, 0,0, 2, PA), pixbit( 0, 1, 0, 88, PB), pixbit( 1, 0, 1, 3, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageFlipH( I , I2 ).
+
 
 FLIP VERTICAL
 EJEMPLO 1:
@@ -61,6 +64,7 @@ pixbit( 0, 0, 0, 10, PA), pixbit( 0, 1, 0, 20, PB), image(1,2,[PA,PB],I), imageF
 EJEMPLO 3:
 pixbit( 0, 0,0, 2, PA), pixbit( 0, 1, 0, 88, PB), pixbit( 1, 0, 1, 3, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageFlipV( I , I2 ).
 
+
 IMAGE CROP
 EJEMPLO 1:
 pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageCrop( I , 0, 0, 1, 1, I2 ).
@@ -69,4 +73,50 @@ pixbit( 0, 0, 0, 10, PA), pixbit( 0, 1, 0, 20, PB), image(1,2,[PA,PB],I), imageC
 EJEMPLO 3:
 pixbit( 0, 0,0, 2, PA), pixbit( 0, 1, 0, 88, PB), pixbit( 1, 0, 1, 3, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageCrop( I , 0, 0, 1, 1, I2 ).
 
+
+IMAGE RGB TO HEX
+EJEMPLO 1:
+pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2,[ P1, P2, P3, P4], I1), imageRGBToHex( I1, I2).
+EJEMPLO 2:
+pixrgb( 0, 0, 48, 20, 255, 10, PA), pixrgb( 0, 1, 150, 22, 154, 20, PB), pixrgb( 1, 0, 17, 170,17, 30, PC), pixrgb( 1, 1, 96, 140, 244, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageCrop(I,0,1,0,1,I2),imageRGBToHex(I2, I3), imageIsHexmap(I3).
+EJEMPLO 3:
+pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image( 2, 2,[ P1, P2, P3, P4], I1), imageRotate90(I1,I2), imageRGBToHex( I2, I3).
+
+
+HISTOGRAMA
+EJEMPLO 1:
+pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageToHistogram( I, Histograma).
+EJEMPLO 2:
+pixhex(0,0,"#FF0000",1,PA), pixhex(0,1,"#00FF00",2,PB), pixhex(1,0,"#0000FF",3,PC), pixhex(1,1,"#FFFFFF",4,PD), image(2,2,[PA,PB,PC,PD],I), imageToHistogram( I, Histograma).
+EJEMPLO 3:
+pixrgb( 0, 0, 10, 10, 10, 10, P1), pixrgb( 0, 1, 20, 20, 20, 20, P2), pixrgb( 1, 0, 30, 30, 30, 30, P3), pixrgb( 1, 1, 40, 40, 40, 40, P4), image(2,2,[P1,P2,P3,P4],I), imageToHistogram( I, Histograma).
+
+
+ROTATE90:
+EJEMPLO 1:
+pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageRotate90( I, I2).
+EJEMPLO 2:
+pixhex( 0, 0, "#0A0A0A", 10, PA), pixhex( 0, 1, "#141414", 20, PB), pixhex( 1, 0, "#1E1E1E", 30, PC), pixhex( 1, 1, "#282828", 40, PD), image( 2, 2, [PA, PB, PC, PD], I), imageRotate90( I, I2).
+EJEMPLO 3:
+pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 0, 20, PB), pixbit( 1, 0, 0, 30, PC), pixbit( 1, 1, 1, 4, PD), image( 2, 2, [PA, PB, PC, PD], I), imageRotate90(I, I2), imageRotate90(I2, I3), imageRotate90(I3, I4), imageRotate90(I4, I5).
+EJEMPLO 4:
+pixbit( 0, 0, 1, 10, PA), pixbit( 0, 1, 1, 10, PB), pixbit( 1, 0, 1, 10, PC), pixbit( 1, 1, 1, 10, PD), image( 2, 2, [PA, PB, PC, PD], I), imageRotate90(I, I2).
+
+IMAGECHANGEPIXEL:
+EJEMPLO 1:  
+pixhex( 0, 0, "FF0058", 10, PA), pixhex( 0, 1, "470850", 20, PB), pixhex( 1, 0, "FFFFFF", 30, PC), pixhex( 1, 1, "444444", 4, PD), image( 2, 2, [PA, PB, PC, PD], I),pixhex( 1, 1, "FAFAFA", 32, MOD),imageChangePixel(I,MOD,I2).
+EJEMPLO 2:
+pixhex( 0, 0, "FF0058", 10, PA), pixhex( 0, 1, "55555", 20, PB), pixhex( 1, 0, "787878", 30, PC), pixhex( 1, 1, "AAAAAA", 4, PD), image( 2, 2, [PA, PB, PC, PD], I),pixhex( 1, 1, "BFBFBF", 32, MOD),imageChangePixel(I,MOD,I2).
+EJEMPLO 3:
+pixhex( 0, 0, "CDCDCD", 10, PA), pixhex( 0, 1, "ABCDEF", 20, PB), pixhex( 1, 0, "FEDCBA", 30, PC), pixhex( 1, 1, "888888", 4, PD), image( 2, 2, [PA, PB, PC, PD], I),pixhex( 1, 1, "BBBBBB", 32, MOD),imageChangePixel(I,MOD,I2).
+
+INVERTCOLORRGB:
+EJEMPLO 1:
+pixbit( 0, 0, 1, 17, PA),imageInvertColorRGB(PA,PIXNUEVO).
+EJEMPLO 2:
+pixrgb( 0, 0, 0,255,125, 17, PA),imageInvertColorRGB(PA,PIXNUEVO).
+EJEMPLO 3:
+pixrgb( 0, 0, 255,0,135, 17, PA),imageInvertColorRGB(PA,PIXNUEVO).
+EJEMPLO 4:
+pixhex( 0, 0, "CDCDCD", 10, PA),imageInvertColorRGB(PA,PIXNUEVO).
 */
--- tdaImage20816739VeraRamirez.pl
+++ tdaImage20816739VeraRamirez.pl
@@ -5,7 +5,7 @@
 
 %TDA IMAGE
 
-:-module(tdaImage20816739VeraRamirez, [image/4, imageIsBitmap/1, imageIsPixmap/1,  imageIsHexmap/1, imageIsCompressed/1 , imageFlipH/2, imageFlipV/2, crop/6]).
+:-module(tdaImage20816739VeraRamirez, [image/4, imageIsBitmap/1, imageIsPixmap/1,  imageIsHexmap/1, imageIsCompressed/1 , imageFlipH/2, imageFlipV/2, imageCrop/6, imageRGBToHex/2, imageToHistogram/2, imageRotate90/2]).
 
 
 :-use_module(tdaPixbit20816739VeraRamirez).
@@ -73,63 +73,259 @@ imageIsCompressed(Image) :-
     Length < Width * Height.
 
 
-
-%Flip Horizontal
+%Pertenencia de flipH
 %Meta Principal: imageFlipH
-%Meta secundaria: flipH
-imageFlipH(Image, FlippedImage) :-
-    image(Width, Height, Data, Image),
+%Meta secundaria: flipH, changeEjeX
+imageFlipH(Image, FlippedImage) :- 
+    image(Width, Height, Data, Image), 
     image(Width, Height, FlippedData, FlippedImage),
-    flipH(Height, 0, Data, FlippedData).
+	%contador para ubicar el n-esimo pixel a buscar en una fila
+    Waux is Width-1, 
+	flipH(Width, 0, Waux, 0, Data, [], FlippedData).
+
+flipH(_, _, _, _, Data, DataFinal, DataFinal):- length(Data,L1), length(DataFinal,L2) , L1==L2,!.
+flipH(Width, CountHeigth, Waux, CountX, Data, DataFinal, FlippedData):-
+    Waux==0, 
+    PosicionPix is CountHeigth + Waux,
+    nth0(PosicionPix,Data,Pixel),
+    changeEjeX(Pixel,CountX,NewPixel),
+    append(DataFinal,[NewPixel],NewDataFinal),
+    NewCountHeigth is CountHeigth + Width,
+    NewWaux is Width - 1,
+    flipH(Width, NewCountHeigth, NewWaux, 0, Data, NewDataFinal, FlippedData),!.
+flipH(Width, CountHeigth, Waux, CountX, Data, DataFinal, FlippedData):-
+    PosicionPix is CountHeigth + Waux,
+    nth0(PosicionPix,Data,Pixel),
+    changeEjeX(Pixel,CountX,NewPixel),
+    append(DataFinal,[NewPixel],NewDataFinal),
+	NewWaux is Waux - 1,
+    NewCountX is CountX + 1,
+    flipH(Width, CountHeigth, NewWaux, NewCountX, Data, NewDataFinal, FlippedData),!.
+
+changeEjeX([EjeY|[_|Ps]],CountX,[EjeY|[CountX|Ps]]).
 
 
-flipH(Height, Counter, Data, FlippedData) :-
-    Counter < Height,
-    nth0(Counter, Data, Row),
-    reverse(Row, FlippedRow),
-    nth0(Counter, FlippedData, FlippedRow),
-    Counter1 is Counter + 1,
-    flipH(Height, Counter1, Data, FlippedData).
-
-flipH(Height, Counter, _, _) :-
-    Counter >= Height.
-
-%Flip Vertical
+%Pertenencia de flipV
 %Meta Principal: imageFlipV
-%Meta secundaria: flipV
-imageFlipV(Image, FlippedImage) :-
-    imageFlipH(Image, FlippedImageH),
-    imageFlipH(FlippedImageH, FlippedImage).
+%Meta secundaria: flipV, changeEjeY
+imageFlipV(Image, FlippedImage) :- 
+    image(Width, Height, Data, Image), 
+    image(Width, Height, FlippedData, FlippedImage),
+    reverse(Data,NewData),
+    Waux is Width-1, 
+	flipV(Width, 0, Waux, 0, NewData, [], FlippedData).  
 
-%Recortar imagen
-%Meta Principal: ImageCrop
-%Meta secundaria: crop
+flipV(_, _, _, _, Data, DataFinal, DataFinal):- length(Data,L1), length(DataFinal,L2) , L1==L2,!.
+flipV(Width, CountHeigth, Waux, CountY, Data, DataFinal, FlippedData):-
+    Waux==0, 
+    PosicionPix is CountHeigth + Waux,
+    nth0(PosicionPix,Data,Pixel),
+    changeEjeY(Pixel,CountY,NewPixel),
+    append(DataFinal,[NewPixel],NewDataFinal),
+    NewCountHeigth is CountHeigth + Width,
+    NewWaux is Width - 1,
+    NewCountY is CountY + 1,
+    flipV(Width, NewCountHeigth, NewWaux, NewCountY, Data, NewDataFinal, FlippedData),!.
+flipV(Width, CountHeigth, Waux, CountY, Data, DataFinal, FlippedData):-    
+	PosicionPix is CountHeigth + Waux,
+    nth0(PosicionPix,Data,Pixel),
+    changeEjeY(Pixel,CountY,NewPixel),
+    append(DataFinal,[NewPixel],NewDataFinal),
+	NewWaux is Waux - 1,
+    flipV(Width, CountHeigth, NewWaux, CountY, Data, NewDataFinal, FlippedData),!.
+
+changeEjeY([_|Ps],CountY,[CountY|Ps]).
+
+
+%Pertenencia de crop
+%Meta Principal: imageCrop
+%Meta secundaria: crop, cropPixels
 imageCrop(Image, X1, Y1, X2, Y2, I2) :-
-    image(Width, Height, Data, Image),
+    image(_, _, Data, Image),
     image(Width, Height, CroppedData, I2),
-    crop(X1, Y1, X2, Y2, Data, CroppedData).
-crop(X1, Y1, X2, Y2, Data, CroppedData) :-
     X1 >= 0,
     Y1 >= 0,
     X2 >= 0,
     Y2 >= 0,
-    X1 =< X2,
-    Y1 =< Y2,
-    crop2(X1, Y1, X2, Y2, Data, CroppedData).
-crop2(X1, Y1, X2, Y2, Data, CroppedData) :-
-    X1 =< X2,
-    Y1 =< Y2,
-    nth0(Y1, Data, Row),
-    nth0(X1, Row, Pixel),
-    nth0(Y1, CroppedData, CroppedRow),
-    nth0(X1, CroppedRow, Pixel),
-    X1_1 is X1 + 1,
-    crop2(X1_1, Y1, X2, Y2, Data, CroppedData).
-crop2(X1, Y1, X2, Y2, Data, CroppedData) :-
-    X1 > X2,
-    Y1 =< Y2,
-    Y1_1 is Y1 + 1,
-    crop2(0, Y1_1, X2, Y2, Data, CroppedData).
-crop2(_, Y1, _, Y2, _, _) :-
-    Y1 > Y2.
+    crop(X1, Y1, X2, Y2, Data, CroppedData, Width, Height).
 
+crop(X1, Y1, X2, Y2, Data, CroppedData, Width, Height) :-
+    X1 =< X2, Y1 =< Y2, 
+    Width is X2 - X1 + 1, Height is Y2 - Y1 + 1, Waux is Width - 1,
+    cropPixels(X1, Y1, X2, Y2, Width, 0, 0, Waux, Data, [], CroppedData),!;
+    X2 =< X1, Y2 =< Y1, 
+    Width is X1 - X2 + 1, Height is Y1 - Y2 + 1, Waux is Width - 1,
+    cropPixels(X2, Y2, X1, Y1, Width, 0, 0, Waux, Data, [], CroppedData),!;
+    X1 =< X2, Y2 =< Y1, 
+    Width is X2 - X1 + 1, Height is Y1 - Y2 + 1, Waux is Width - 1,
+    cropPixels(X1, Y2, X2, Y1, Width, 0, 0, Waux, Data, [], CroppedData),!;
+    Width is X1 - X2 + 1, Height is Y2 - Y1 + 1, Waux is Width - 1,
+    cropPixels(X2, Y1, X1, Y2, Width, 0, 0, Waux, Data, [], CroppedData),!.   
+
+cropPixels(_, _, X, Y, _, CountX, CountY, _, [[Y|[X|RestoPix]]|_], FinalData, CroppedData) :-
+    append(FinalData, [[CountY|[CountX|RestoPix]]], CroppedData),!.
+
+cropPixels(X1, Y1, X2, Y2, Width, CountX, CountY, Waux, [[Y|[X|RestoPix]]|RestoData], FinalData, CroppedData) :-
+	X >= X1, X =< X2, Y >= Y1, Y =< Y2,
+    append(FinalData, [[CountY|[CountX|RestoPix]]], NewFinalData),
+    Waux == 0,
+    Waux2 is Width - 1,
+    CountY2 is CountY + 1,
+    cropPixels(X1, Y1, X2, Y2, Width, 0, CountY2, Waux2, RestoData, NewFinalData, CroppedData),!;
+    X >= X1, X =< X2, Y >= Y1, Y =< Y2,
+    append(FinalData, [[CountY|[CountX|RestoPix]]], NewFinalData),
+    Waux2 is Waux - 1,
+    CountX2 is CountX + 1,
+    cropPixels(X1, Y1, X2, Y2, Width, CountX2, CountY, Waux2, RestoData, NewFinalData, CroppedData),!.
+    
+cropPixels(X1, Y1, X2, Y2, Width, CountX, CountY, Waux, [_|RestoData], FinalData, CroppedData) :-
+    cropPixels(X1, Y1, X2, Y2, Width, CountX, CountY, Waux, RestoData, FinalData, CroppedData).
+    
+	
+%Pertenencia de RGBtoHex
+%Meta Principal: imageRGBToHex
+%Meta secundaria: conversionImagen, conversionPixel, conversionColor
+imageRGBToHex(I,I2):- 
+    imageIsPixmap(I), 
+    image(Width, Height, Data, I), 
+    image(Width, Height, RGBToHexData, I2),
+    append([], ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"], ConversionDigitos),
+	conversionImagen(Data, [], ConversionDigitos, RGBToHexData).
+
+conversionImagen([], NewData, _, NewData).
+conversionImagen([RGBPixel|RestoData], NewData, ConversionDigitos, RGBToHexData):-
+    conversionPixel(RGBPixel, ConversionDigitos, HexPixel),
+    append(NewData, [HexPixel], NewData2),
+    conversionImagen(RestoData, NewData2, ConversionDigitos, RGBToHexData),!.
+
+conversionPixel([Y|[X|[R|[G|[B|D]]]]], ConversionDigitos, [Y|[X|[Hex|D]]]):-
+    conversionColor(R, ConversionDigitos, RedHex),
+    conversionColor(G, ConversionDigitos, GreenHex),
+    conversionColor(B, ConversionDigitos, BlueHex),
+    atomic_list_concat(["#", RedHex, GreenHex, BlueHex], "", ColorHex),
+    string_concat(ColorHex, "", Hex).
+
+conversionColor(ColorRGB, ConversionDigitos, ColorHex):-
+    Div is ColorRGB // 16,
+    Rem is ColorRGB mod 16,
+    nth0(Div, ConversionDigitos, N1),
+    nth0(Rem, ConversionDigitos, N2),
+	atomic_list_concat([N1, N2], "", ColorHex).
+
+
+%Pertenencia de histograma
+%Meta Principal: imageToHistogram
+%Meta secundaria: histogramaBitmap, histogramaPixmap, histogramaHexmap, buscarColorHistograma
+imageToHistogram(Image, Histograma):-
+    imageIsBitmap(Image),
+    image(_, _, Data, Image), 
+	histogramaBitmap(Data, [], Histograma),!;
+    imageIsPixmap(Image),
+    image(_, _, Data, Image), 
+	histogramaPixmap(Data, [], Histograma),!;
+    imageIsHexmap(Image),
+    image(_, _, Data, Image), 
+	histogramaHexmap(Data, [], Histograma),!.
+
+histogramaBitmap([], Histograma, Histograma).
+histogramaBitmap([Pixel|Resto], Histograma, FinalHistograma):-
+    pixbit(_, _, Bit, _, Pixel),
+    buscarColorHistograma(Bit, Histograma, Posicion, Count),
+    Count2 is Count + 1,
+    nth0(Posicion, Histograma, _, RestoHistograma),
+    append(RestoHistograma, [[Bit, Count2]], NewHistograma),
+    histogramaBitmap(Resto, NewHistograma, FinalHistograma),!;
+    pixbit(_, _, Bit, _, Pixel),
+    append(Histograma, [[Bit, 1]], NewHistograma),
+    histogramaBitmap(Resto, NewHistograma, FinalHistograma),!.
+    
+histogramaPixmap([], Histograma, Histograma).
+histogramaPixmap([Pixel|Resto], Histograma, FinalHistograma):-
+    pixrgb(_, _, R, G, B, _, Pixel),
+    buscarColorHistograma([R, G, B], Histograma, Posicion, Count),
+    Count2 is Count + 1,
+    nth0(Posicion, Histograma, _, RestoHistograma),
+    append(RestoHistograma, [[[R, G, B], Count2]], NewHistograma),
+    histogramaPixmap(Resto, NewHistograma, FinalHistograma),!;
+    pixrgb(_, _, R, G, B, _, Pixel),
+    append(Histograma, [[[R, G, B], 1]], NewHistograma),
+    histogramaPixmap(Resto, NewHistograma, FinalHistograma),!.
+
+histogramaHexmap([], Histograma, Histograma).
+histogramaHexmap([Pixel|Resto], Histograma, FinalHistograma):-
+    pixhex(_, _, Hex, _, Pixel),
+    buscarColorHistograma(Hex, Histograma, Posicion, Count),
+    Count2 is Count + 1,
+    nth0(Posicion, Histograma, _, RestoHistograma),
+    append(RestoHistograma, [[Hex, Count2]], NewHistograma),
+    histogramaHexmap(Resto, NewHistograma, FinalHistograma),!;
+    pixhex(_, _, Hex, _, Pixel),
+    append(Histograma, [[Hex, 1]], NewHistograma),
+    histogramaHexmap(Resto, NewHistograma, FinalHistograma),!.
+
+buscarColorHistograma(Color, Histograma, Posicion, Count):-
+    nth0(Posicion, Histograma, [Color,Count]).
+
+
+%Pertenencia de rotate90
+%Meta Principal: imageRotate90
+%Meta secundaria: rotate90, changeEjesRotate
+imageRotate90(Image, RotatedImage):-
+    image(Width, Height, Data, Image),
+    image(Width, Height, RotatedData, RotatedImage),
+    CountRows is Height - 1,
+    NVertice is CountRows * Width,
+    rotate90(Width, Height, NVertice, CountRows, 0, 0, Data, [], RotatedData).
+
+rotate90(_, _, _, _, _, _, Data, FinalData, FinalData):- length(Data,L1), length(FinalData,L2) , L1==L2,!.
+rotate90(Width, Height, NVertice, CountRows, CountCol, CountAux, Data, FinalData, RotatedData):-
+    CountRows == 0,
+    nth0(NVertice,Data,Pixel), 
+    changeEjesRotate(Pixel, CountAux, CountCol, NewPixel),
+    append(FinalData,[NewPixel],NewDataFinal),
+    CountCol2 is CountCol + 1,
+    Haux is Height - 1,
+    NVerticeAux is Haux * Width,
+    NVertice2 is NVerticeAux + CountCol2,
+    rotate90(Width, Height, NVertice2, Haux, CountCol2, 0, Data, NewDataFinal, RotatedData),!.
+        
+rotate90(Width, Height, NVertice, CountRows, CountCol, CountAux, Data, FinalData, RotatedData):-
+    nth0(NVertice,Data,Pixel), 
+    changeEjesRotate(Pixel, CountAux, CountCol, NewPixel),
+    append(FinalData,[NewPixel],NewDataFinal),
+    NVertice2 is NVertice - Width,
+    CountRows2 is CountRows - 1,
+    CountAux2 is CountAux + 1,
+    rotate90(Width, Height, NVertice2, CountRows2, CountCol, CountAux2, Data, NewDataFinal, RotatedData),!.
+    
+changeEjesRotate([_|[_|RestoPixel]], NewX, NewY, [NewY|[NewX|RestoPixel]]).
+
+%Pertenencia de imageChangePixel
+%Meta Principal: imageChangePixel
+%Meta secundaria: changePixel2
+imageChangePixel(I, Pixel, RotatedImage):-
+    image(Width, Height, Data, I),
+    image(Width, Height, RotatedData, RotatedImage),
+	changePixel2(Data, Pixel, [], RotatedData).
+
+changePixel2([], _, FinalData, FinalData). 
+
+changePixel2([[Y|[X|_]]|RestoPixeles], [Y|[X|RestoP2]], FinalData, RotatedData):- 
+    append(FinalData, [[Y|[X|RestoP2]]], NewDataFinal),
+    changePixel2(RestoPixeles, [Y|[X|RestoP2]], NewDataFinal, RotatedData),!.
+
+changePixel2([PixelC|RestoPixeles], P2, FinalData, RotatedData):- 
+    append(FinalData, [PixelC], NewDataFinal),
+    changePixel2(RestoPixeles, P2, NewDataFinal, RotatedData),!.
+
+
+
+%Modificador de invertir RGB
+%Meta Principal: imageInvertColorRGB
+%Meta secundaria: pixrgb
+imageInvertColorRGB(Pixel,PixelModificado):- 
+    pixrgb(Y,X,RED,GREEN,BLUE,Depth, Pixel),
+ pixrgb(Y,X,REDinvertido,GREENinvertido,BLUEinvertido,Depth, PixelModificado),
+ REDinvertido is abs(255-RED), 
+ GREENinvertido is abs(255-GREEN), 
+ BLUEinvertido is abs(255-BLUE),!.
